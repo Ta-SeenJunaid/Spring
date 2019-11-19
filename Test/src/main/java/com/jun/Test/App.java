a3359713f370e986f59b554a6b6b63a09cc5a7a7
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-11-19 23:15:15 +0600

    App.java is added

---
+++ Test/src/main/java/com/jun/Test/App.java
@@ -0,0 +1,13 @@
+package com.jun.Test;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Coach theCoach = new TrackCoach();
+    	
+    	System.out.println(theCoach.getDailyWorkout());
+
+    }
+}
